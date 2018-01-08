4a5c48346226e1e82a1a95fb8752779bd9b27ef7
Dieudonne AMOIKON <delrodieamoikon@gmail.com>
2018-01-08 04:27:14 +0000

    Mise en page de la classe Departement2

--- src/AppBundle/Entity/Departement2.php
+++ src/AppBundle/Entity/Departement2.php
@@ -69,7 +69,7 @@ class Departement2
     /**
      * NOTE: This is not a mapped field of entity metadata, just a simple property.
      *
-     * @Vich\UploadableField(mapping="actualite_image", fileNameProperty="imageName", size="imageSize")
+     * @Vich\UploadableField(mapping="departement_image", fileNameProperty="imageName", size="imageSize")
      *
      * @var File
      */
@@ -337,6 +337,38 @@ class Departement2
     public function getUpdatedAt()
     {
         return $this->updatedAt;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the  update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $image
+     *
+     * @return Post
+     */
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        if ($image) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return File|null
+     */
+    public function getImageFile()
+    {
+        return $this->imageFile;
     }
 
     /**
