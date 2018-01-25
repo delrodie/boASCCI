efecd4851fff36a78bf64cefc6ebc8cccac662e5
Dieudonne AMOIKON <delrodieamoikon@gmail.com>
2018-01-25 04:45:58 +0000

    Correction nombre de slide

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,7 +15,7 @@ class DefaultController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
 
-        $sliders = $em->getRepository('AppBundle:Slider')->findSlideStandard(0, 4); //dump($sliders);die();
+        $sliders = $em->getRepository('AppBundle:Slider')->findSlideStandard(0, 5); //dump($sliders);die();
         $slideUniques = $em->getRepository('AppBundle:Slider')->findOneSlide(5, 1); //dump($slideUnique);die();
         $envolIntros = $em->getRepository('AppBundle:Envol')->findOneEnvol(0, 1); //dump($envolIntro);die();
         $envols = $em->getRepository('AppBundle:Envol')->findEnvol(); //dump($envols);die();
